c761786cce1c0a8c8be5469a5d99846049624c72
Artur Santos <67513898+Artur-STN@users.noreply.github.com>
2022-11-23 13:34:48 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+document.getElemrnyId('quantidade_Pac').value = 0
+document.getElemrnyId('quantidade_Sedex').value = 0
+
 function maisUm(tipo_de_envio) {
     
     let input_number = document.getElementById(`quantidade_` + tipo_de_envio)
